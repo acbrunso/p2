27be1e8eaaf556d27b1a21d2831d2823404880f7
acbrunso <brunsonac@gmail.com>
2015-09-19 00:04:12 -0500

    added check.php

--- check.php
+++ check.php
@@ -1,10 +1,10 @@
 
 <?php
-  $currentPage = "$_SERVER[REQUEST_URI]";
-  /*if($currentPage!="/index.php") {
+  $currentPage = substr("$_SERVER[REQUEST_URI]", 0, 10);
+  if($currentPage!="/index.php" || $currentPage!="/" ) {
     header("Location: http://p2.anthonybrunson.me");
     die();
   }
-  */
+
   echo $currentPage
 ?>
