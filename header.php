f9e04ddfd908569022421134546ea3790e3bffd3
acbrunso <brunsonac@gmail.com>
2015-09-18 23:31:10 -0500

    fixed errors.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <h1>Password Generator</h1>
   </div>
 
-  <div id="nav top">
+  <div id="nav" class="top">
     <ul>
       <li><a href="index.php?thePage=home">Home</a></li>
       <li><a href="index.php?thePage=about">About</a></li>
